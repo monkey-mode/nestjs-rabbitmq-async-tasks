606ea4c1c475426cb7c2e180328f4fb4551eebe9
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2023-12-16 19:47:52 +0700

    Add User Endpoint And Add Create User Entities

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(newUser);
+  }
+}
